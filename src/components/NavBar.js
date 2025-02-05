f915d5f3a08b6676a50ed06dcc98e660dccb818c
zahrafeisal <zahrafeisal3@gmail.com>
2025-02-06 02:43:51 +0300

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,10 @@ import React from "react";
 function NavBar() {
     return (
         <nav>
+            <a href="/personal/home">Home</a>
             <ul>
                 <li>
+                    <a href="/personal/about">Text</a>
                     <a href="/personal/">Log out</a>
                 </li>
             </ul>
